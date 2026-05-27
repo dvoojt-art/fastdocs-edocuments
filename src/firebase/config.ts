a9c911156479031fefb84bde84b6ca97a5ad7c16
Daryl Bayonla <bayonladaryljohn@gmail.com>
2026-05-27 02:48:40 +0000

    apiKey: "AIzaSyB0y5nyRc0WUz2pAbacB2ZugmprYSXfLaw"

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "placeholder-key",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyB0y5nyRc0WUz2pAbacB2ZugmprYSXfLaw",
+  authDomain: "studio-7833616261-22160.firebaseapp.com",
+  projectId: "studio-7833616261-22160",
+  storageBucket: "studio-7833616261-22160.firebasestorage.app",
+  messagingSenderId: "343543012759",
+  appId: "1:343543012759:web:0c4731dbff3bc766f4b8d2"
 };
